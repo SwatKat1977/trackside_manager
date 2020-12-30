f3fd516dd47e399d3e6cf364c777c0bdf5071ef0
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-12-30 14:44:04 +0000

    changes

---
+++ src/trackside_master/env.py
@@ -0,0 +1,12 @@
+'''
+Copyright (C) 2020 Trackside Manager Project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+'''
+import sys
+sys.path.insert(0,'.')
+
+MASTER_VERSION = 'v0.0.0'
--- src/trackside_master/prototype.py
+++ src/trackside_master/prototype.py
@@ -6,15 +6,28 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 '''
-from framework import Framework
+import env
+from common.framework import Framework
 
 class App(Framework):
+    """ tbd """
+
     def __init__(self):
         super().__init__()
+        self._app = None
+
+    def _initialise(self):
+        print('_initialise() called')
+
+    def _main_loop(self):
+        print('_initialise() called')
 
 def main():
+    """ Main entry point """
+
     app = App()
 
+    print(f'Master version: {env.MASTER_VERSION}')
     print(app.is_running)
 
 main()
