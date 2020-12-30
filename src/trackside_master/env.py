349f6e8d031105e33bb7de68ab456a6b7e85b8b1
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-12-30 21:00:05 +0000

    Merge development into main (#3)
    * Initial Prototyping Design (#1)
    
    * prototype placeholder
    
    * added doxygen configuration
    
    * changes to framework
    
    * work on gitignore
    
    * move
    
    * move
    
    * work on pipelines
    
    * remove
    
    * changes
    
    * GitHub files (#2)
    
    * Create pylint action for master
    
    Master pylint
    
    * Update pylint for master
    
    * github files
    
    * clean up
    
    * fixed branch name
    
    * update pull request template

---
+++ src/trackside_master/env.py
@@ -0,0 +1,13 @@
+'''
+Copyright (C) 2020 Trackside Manager Project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+'''
+import sys
+sys.path.insert(0,'.')
+
+##  Version of the Trackside Master service
+MASTER_VERSION = 'v0.0.0'
