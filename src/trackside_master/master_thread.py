cb70c44bd8459c97013c21596c470c34196a17e8
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-01-05 13:50:19 +0000

    wip

--- src/trackside_master/master_thread.py
+++ src/trackside_master/master_thread.py
@@ -6,6 +6,21 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 '''
+from common.logger import LogType
+from common.service_thread import ServiceThread
 
-class MasterThread:
-    pass
+class MasterThread(ServiceThread):
+
+    def __init__(self, logger):
+        super().__init__()
+        self._logger = logger
+
+    def initialise(self) -> bool:
+        self._logger.log(LogType.Info, 'init')
+
+        self._is_initialised = True
+
+        return True
+
+    def _main_loop(self) -> None:
+        pass
