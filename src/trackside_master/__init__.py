f1f9d0624768773e08eb617f9323dfd83e40a368
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-01-13 22:25:48 +0000

    change copyright

--- src/trackside_master/__init__.py
+++ src/trackside_master/__init__.py
@@ -1,5 +1,5 @@
 '''
-Copyright (C) 2020 Trackside Manager Project
+Copyright (C) 2020-2021 Trackside Manager Project
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
