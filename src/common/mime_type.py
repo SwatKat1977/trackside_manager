5e75bb7179bffd00841089ddcfa60c5c4101c476
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2021-01-14 10:33:13 +0000

    added common code

---
+++ src/common/mime_type.py
@@ -0,0 +1,54 @@
+'''
+Copyright (C) 2020-2021 Trackside Manager Project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+'''
+
+class MIMEType:
+    """ Definition of MIME types """
+    # pylint: disable=too-few-public-methods
+
+    # .css | Cascading Style Sheets (CSS)
+    CascadingStyleSheets = 'text/css'
+
+    # .csv | Comma-separated values (CSV)
+    CommaSeparatedValues = 'text/csv'
+
+    # .gif |	Graphics Interchange Format (GIF)
+    GIF = 'image/gif'
+
+    # .htm, .html | HyperText Markup Language (HTML)
+    HTML = 'text/html'
+
+    # .jpeg, .jpg | JPEG images
+    JPEG = 'image/jpeg'
+
+    # .js | JavaScript
+    JavaScript = 'text/javascript'
+
+    # .json | JSON format
+    JSON = 'application/json'
+
+    # .jsonld | JSON-LD format
+    JSONLD = 'application/ld+json'
+
+    # .mp3| MP3 audio
+    MP3 = 'audio/mpeg'
+
+    # .mpeg | MPEG Video
+    MPEGVideo = 'video/mpeg'
+
+    # .png | Portable Network Graphics
+    PNG = 'image/png'
+
+    # .pdf | Adobe Portable Document Format (PDF)
+    PDF = 'application/pdf'
+
+    # .txt | Text, (generally ASCII or ISO 8859-n)
+    Text = 'text/plain'
+
+    # .xml | eXtensible Markup Language (XML)
+    XM = 'application/xml'
